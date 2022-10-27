a0af1432224c08f892641c66c558231fa97c8f11
renatolew <renato.lew@gmail.com>
2022-10-27 18:00:25 -0300

    Practicing variable type conversion in Go.

--- basics7.go
+++ basics7.go
@@ -4,7 +4,7 @@ import "fmt"
 
 type hotdog int // Creating a type to explore atributions and comparisons with created types.
 
-var b hotdog = 10
+var b hotdog = 101
 
 func main() {
 	x := 10
@@ -12,7 +12,11 @@ func main() {
 	fmt.Printf("%T\n\n", b)
 
 	fmt.Printf("%v\n", x)
-	fmt.Printf("%v", b)
+	fmt.Printf("%v\n\n", b)
 
 	// b = x -> can't do this because they are of different types. Even tho b receives int values, its type is hotdog, not int.
+
+	x = int(b) // now I can convert variable types by declaring it in the atribution
+	fmt.Printf("%v\n", x)
+
 }
