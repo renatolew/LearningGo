370d36b6a793f2f782d2037ea0a9f06614e27e85
renatolew <renato.lew@gmail.com>
2022-10-28 16:10:34 -0300

    First exercise: practicing declaring variables and printing statements in different ways.

---
+++ exercise1.go
@@ -0,0 +1,24 @@
+// - Utilizando o operador curto de declaração, atribua estes valores às variáveis com os identificadores "x", "y", e "z".
+// 1. 42
+// 2. "James Bond"
+// 3. true
+// - Agora demonstre os valores nestas variáveis, com:
+// 1. Uma única declaração print
+// 2. Múltiplas declarações print
+
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println("Variable 'x': ", x)
+	fmt.Println("Variable 'y': ", y)
+	fmt.Println("Variable 'z': ", z)
+
+	fmt.Println("\nVariable 'x' is: ", x, ". Variable 'y' is: ", y, ". And variable 'z' is: ", z, ".")
+}
